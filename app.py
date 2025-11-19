fa023aac72f717d8610e4053864df95f7df3e41c
rafa5115 <59081061+rafa5115@users.noreply.github.com>
2025-11-19 03:07:08 -0300

    Update app.py

--- app.py
+++ app.py
@@ -105,4 +105,5 @@ def registrar(prefixo, id):
 # --------------------------------------------------
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8080))
-    app.run(host="0.0.0.0", port=port)
+    app.run(host="127.0.0.1", port=8080)
+
